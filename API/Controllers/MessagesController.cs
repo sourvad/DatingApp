2ad73ad553f68263e8a9714dc457588e979d22dd
Sourab Vadlamani <sourvad@gmail.com>
2023-07-08 13:18:24 -0700

    End of section 18, added UnitOfWork and cleaed up client

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -11,72 +11,31 @@ namespace API.Controllers
     public class MessagesController : BaseApiController
     {
         private readonly IMapper _mapper;
-        private readonly IMessageRepository _messageRepository;
-        private readonly IUserRepository _userRepository;
-        public MessagesController(IUserRepository userRepository, IMessageRepository messageRepository, IMapper mapper)
+        private readonly IUnitOfWork _uow;
+
+        public MessagesController(IUnitOfWork uow, IMapper mapper)
         {
-            _userRepository = userRepository;
-            _messageRepository = messageRepository;
+            _uow = uow;
             _mapper = mapper;
         }
-
-        // TODO: Remove commented code
-        // [HttpPost]
-        // public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
-        // {
-        //     var username = User.GetUsername();
-
-        //     if (username == createMessageDto.RecipientUsername.ToLower()) return BadRequest("You cannot send messages to yourself");
-
-        //     if (String.IsNullOrWhiteSpace(createMessageDto.Content)) return BadRequest("You cannot send an empty or only whitespace message");
-
-        //     var sender = await _userRepository.GetUserByUsernameAsync(username);
-        //     var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
-
-        //     if (recipient == null) return NotFound();
-
-        //     var message = new Message
-        //     {
-        //         Sender = sender,
-        //         Recipient = recipient,
-        //         SenderUsername = sender.UserName,
-        //         RecipientUsername = recipient.UserName,
-        //         Content = createMessageDto.Content
-        //     };
-
-        //     _messageRepository.AddMessage(message);
-            
-        //     // TODO: Return CreatedAtAction
-        //     if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
-
-        //     return BadRequest("Failed to send message");
-        // }
 
         [HttpGet]
         public async Task<ActionResult<PagedList<MessageDto>>> GetMessageForUser([FromQuery]MessageParams messageParams)
         {
             messageParams.Username = User.GetUsername();
 
-            var messages = await _messageRepository.GetMessagesForUser(messageParams);
+            var messages = await _uow.MessageRepository.GetMessagesForUser(messageParams);
 
             Response.AddPaginationHeader(new PaginationHeader(messages.CurrentPage, messages.PageSize, messages.TotalCount, messages.TotalPages));
 
             return messages;
         }
 
-        [HttpGet("thread/{username}")]
-        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
-        {
-            var currentUserName = User.GetUsername();
-
-            return Ok(await _messageRepository.GetMessageThread(currentUserName, username));
-        }
-
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteMessage(int id)
         {
             var username = User.GetUsername();
-            var message = await _messageRepository.GetMessage(id);
+            var message = await _uow.MessageRepository.GetMessage(id);
             
             if (message.SenderUsername == username) message.SenderDeleted = true;
             else if (message.RecipientUsername == username) message.RecipientDeleted = true;
@@ -84,10 +43,10 @@ namespace API.Controllers
 
             if (message.SenderDeleted && message.RecipientDeleted)
             {
-                _messageRepository.DeleteMessage(message);
+                _uow.MessageRepository.DeleteMessage(message);
             }
 
-            if (await _messageRepository.SaveAllAsync()) return Ok();
+            if (await _uow.Complete()) return Ok();
 
             return BadRequest("Problem deleting the message");
         }
